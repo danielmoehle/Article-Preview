405e2f6831da45b2b51567fd85fb79fa2953f554
danielmoehle <107589317+danielmoehle@users.noreply.github.com>
2024-08-07 13:31:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var panel = document.getElementsByClassName("share")
+var x = document.getElementById("share-panel")
+
+function myFunction() {
+        if (x.style.display == "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
